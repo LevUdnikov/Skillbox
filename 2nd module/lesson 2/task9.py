cb6391cd85caca7b7bdb664c2f49762dc3f4f63a
Илья Шубин <shubin152@gmail.com>
2023-11-02 19:37:29 +0500

    Update task9.py

--- 2nd module/lesson 2/task9.py
+++ 2nd module/lesson 2/task9.py
@@ -1,5 +1,5 @@
-N = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+N = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 2]  # Но тут может быть любой список
 
-for index in range(len(N)-1, -1, -1):
+for index in range(len(N) - 1, -1, -1):
     if N[index] % 2 == 0:
         print(N[index], end=" ")
